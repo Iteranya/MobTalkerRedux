495ca2da630dcd29872033521c65ab668d530601
Ars Paradox <152275094+Iteranya@users.noreply.github.com>
2024-10-23 09:09:46 +0700

    Added more into example chat

--- src/main/java/org/arsparadox/mobtalkerredux/HelloWorldItem.java
+++ src/main/java/org/arsparadox/mobtalkerredux/HelloWorldItem.java
@@ -28,12 +28,20 @@ public class HelloWorldItem extends Item {
                     new Choice("Tell me more about this place.", 0, 3),
                     new Choice("Just passing by, thank you.", 0, 4)
             ),null);
-            Dialogue answer1 = new Dialogue(3,"This land is filled with wonders and dangers alike. Be cautious!", List.of(),5);
-            Dialogue answer2 = new Dialogue(4,"Ah, passing by then? Have fun! I bid you adieu", List.of(),5);
+            Dialogue answer1 = new Dialogue(3,"This land is filled with wonders and dangers alike. Be cautious!", List.of(),6);
+            Dialogue answer2 = new Dialogue(4,"Ah, passing by then? Have fun! I bid you adieu", List.of(),null);
             Dialogue goodbye = new Dialogue(5,"Safe travels, stranger! Remember, you're always welcome here.", List.of(),null);
+            Dialogue question2 = new Dialogue(6,"Anyway, can you fight?", List.of(
+                    new Choice("Yep", 0, 7),
+                    new Choice("Nope", 0, 8),
+                    new Choice("Maybe?", 0, 9)
+            ),null);
+            Dialogue answer3 = new Dialogue(7,"Great! Have fun then!", List.of(),null);
+            Dialogue answer4 = new Dialogue(8,"Good luck lmao", List.of(),null);
+            Dialogue answer5 = new Dialogue(9,"That's good enough~", List.of(),null);
             // Create a DialogueManager with the example dialogues
             DialogueManager dialogueManager = new DialogueManager(
-                    List.of(start,greeting, question, answer1, answer2, goodbye)
+                    List.of(start,greeting, question, question2,answer1, answer2, goodbye,answer3,answer4,answer5)
             );
             Minecraft.getInstance().execute(() -> {
                         Minecraft.getInstance().setScreen(new DialogueScreen(dialogueManager));
